e963d5acba3234c8c2904ef73f2c569a44f43fc9
Srikar Gouru <srikarg89@gmail.com>
2019-11-28 20:20:02 -0500

    Confirmed sim's calculations lines up with physics

--- simulation/ball_drop.py
+++ simulation/ball_drop.py
@@ -6,15 +6,28 @@ ball = world.add_sphere(radius=1, mass=1)
 ground = world.add_ground()
 
 ball.set_position(0, 0, 20)
+gravity = world.get_gravity()
+print("Gravity is", gravity)
 print(ball.get_position())
+print("Initial total energy is", ball.get_energy(gravity))
 
-time_step = 0
+total = 0
+time_step = 0.002
 
 while ball.get_position()[2] > ball.get_radius():
-    print("Vertical position of ball:", ball.get_position()[2])
-    print("Vertical velocity of ball", ball.get_linear_velocity()[2])
+#    print("Vertical position of ball:", ball.get_position()[2])
+#    print("Vertical velocity of ball", ball.get_linear_velocity()[2])
     world.set_time_step(time_step)
     world.integrate()
-    time_step += 0.002
+    total += time_step
+#    time_step += 0.002
 
-print(time_step)
+print("Time:", total)
+vel = abs(ball.get_linear_velocity()[2])
+print("Calculcated velocity (with kinematics)", (2 * abs(gravity[2]) * 20) ** (1/2))
+print("Calculated velocity (also with kinematics)", gravity[2] * total)
+print("Measured velocity", vel)
+print("Calculated KE (from velocity)", vel ** 2 / 2)
+print("Measured KE", ball.get_kinetic_energy())
+print("Measured PE", ball.get_potential_energy(gravity))
+print("Final total energy is", ball.get_energy(gravity))
