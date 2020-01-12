61464caa70b7426f485b7aeed610441fb59e44bc
JayChen35 <jay.chen135@gmail.com>
2020-01-12 02:34:53 -0500

    Add i2c sensor arduino test code

--- src/sensors/sensors-i2c-test/sensors-i2c-test.ino
+++ src/sensors/sensors-i2c-test/sensors-i2c-test.ino
@@ -1,13 +1,16 @@
 #include <Wire.h>
 #define SLAVE_ADDRESS 0x40
+#define FLOATS_SENT 2
 
 int trigPinX = 11;    // Trigger
 int echoPinX = 12;    // Echo
 int trigPinY = 10;    // Trigger
 int echoPinY = 9;    // Echo
 String cmd;
-String reply;
- 
+float cmX;
+float cmY;
+float data[FLOATS_SENT];
+
 void setup() {
   //Serial Port begin
   Serial.begin (9600);
@@ -20,7 +23,7 @@ void setup() {
   Wire.begin(SLAVE_ADDRESS);
   // Define method to call when something is recieved
   Wire.onReceive(receiveData);
-  // Wire.onRequest(requestData);
+  // Wire.onRequest(recieveData);
 }
  
 void loop() {
@@ -41,8 +44,10 @@ void receiveData(int byteCount) {
   // of the ping to the reception of its echo off of an object.
   pinMode(echoPinX, INPUT);
   float durationX = pulseIn(echoPinX, HIGH);
-  float cmX = (durationX/2) / 74;
+  cmX = (durationX/2) / 74;
+  data[0] = cmX;
   float inchesX = (durationX/2) / 74;   // Divide by 74 or multiply by 0.0135
+  // Serial.println(cmX);
 
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
@@ -57,21 +62,27 @@ void receiveData(int byteCount) {
   // of the ping to the reception of its echo off of an object.
   pinMode(echoPinY, INPUT);
   float durationY = pulseIn(echoPinY, HIGH);
-  float cmY = (durationY/2) / 74;
+  cmY = (durationY/2) / 74;
+  data[1] = cmY;
   float inchesY = (durationY/2) / 74;   // Divide by 74 or multiply by 0.0135
+  // Serial.println(cmY);
 
   while (Wire.available()) {
     cmd = (char)Wire.read();
   }
   if (cmd == "send_ultrasonic_data") {
-    union cvt {
-      float val;
-      unsigned char byte_array[4];
-    } x;
-    x.val = cmX;
-    Wire.write(x.byte_array, 4);
+    Serial.println("Cmd recieved");
   }
-  else {
-    Wire.send("Command failed.");
-  }
+//  union cvt {
+//    float val;
+//    unsigned char byte_array[4];
+//  } x;
+//  x.val = cmX;
+//  Serial.println(x.val);
+//  Wire.write(x.byte_array, 4);
+  // Wire.write((byte*) &data, FLOATS_SENT*sizeof(float));
+  // byte data[4] = {0x64, 0xd8, 0x64, 0x3f};
+  // Wire.write(data, 4);
+  Wire.write(1)
+  Serial.println("Reply sent.");
 }
