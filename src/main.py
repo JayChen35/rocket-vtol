441c44e336d8d3a2854af08810342c4d1bcee434
= <jay.chen135@gmail.com>
2020-01-19 03:57:58 +0000

    Yaboi i2c works

--- src/main.py
+++ src/main.py
@@ -1,31 +1,19 @@
-import smbus2
 import smbus
-from smbus2 import SMBusWrapper 
+#from smbus2 import SMBusWrapper 
 import time
 import struct
  
-# Slave Addresses for Arduinos 
-SLAVE_ADDRESS = 0x40
- 
+# Slave Addresses for Arduinos
+SLAVE_ADDRESS = 0x4
+bus = smbus.SMBus(1)
+
 # Create the I2C bus 
 while True:
-    with SMBusWrapper(1) as bus:
-        cmd = "send_ultrasonic_data"
-        # byte_array = str_to_byte_array(cmd)
-        byte_array = cmd.encode("utf-8")
-        bus.write_i2c_block_data(SLAVE_ADDRESS, 0x10, byte_array)
-        print("Data sent.")
-        time.sleep(0.1)
-        # block = bus.read_i2c_block_data(SLAVE_ADDRESS, 0, 4)
-        # print(block)
-        # data = struct.unpack('f', bytearray(block))[0]
-        # data = struct.unpack('<l', block)
-        # print(data)
-        data = bus.read_byte(SLAVE_ADDRESS, 0)
-        print(data)
-        example = b"\x64\xd8\x64\x3f"
-        print(struct.unpack('f', example)[0])
-    time.sleep(1)
+    block = bus.read_i2c_block_data(SLAVE_ADDRESS, 0x10, 4)
+    ultra1 = float(str(block[0]) + '.' + str(block[1]).zfill(2))
+    ultra2 = float(str(block[2]) + '.' + str(block[3]).zfill(2))
+    print(str(ultra1) + " " + str(ultra2))
+    time.sleep(.1)
 
 """
 bus = smbus.SMBus(1)
