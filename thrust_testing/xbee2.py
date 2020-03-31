947e28fde1c0c965268a3123d2796624a86980e5
Srikar Gouru <srikarg89@gmail.com>
2020-03-31 16:21:01 -0400

    Delete stuff

--- thrust_testing/xbee2.py
+++
@@ -1,7 +0,0 @@
-import serial
-import time
-
-ser = serial.Serial('COM17', baudrate=9600, timeout=0)
-while True:
-    ser.write(b"hi")
-    time.sleep(1)
--- thrust_testing/xbee_test.py
+++
@@ -1,7 +0,0 @@
-import serial
-import time
-
-ser = serial.Serial('COM15', baudrate=9600, timeout=0)
-while True:
-    inp = input()
-    ser.write(inp.encode())
